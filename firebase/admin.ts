0c72f01c8862bc88b2c657603e97d7f2ef813cb3
Bhakti <bhaktinimaj94@gmail.com>
2025-07-29 19:30:59 +0530

    Fix: added Firebase service account setup

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,16 +1,13 @@
-// FILE: firebase/admin.ts
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
 const initFirebaseAdmin = () => {
     if (!getApps().length) {
+        const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS || '{}');
+
         initializeApp({
-            credential: cert({
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // ✅ fix newline
-            }),
+            credential: cert(serviceAccount),
         });
     }
 
